dfd3eadfe44f98fdd7db3f6eeb7bef33138ccf4a
Jonáš <jonas.hlinak16@gmail.com>
2026-03-10 15:32:54 +0100

    Refactor Lss methods and improve code structure

--- Linearni_spojovy_seznam/Lss.cpp
+++ Linearni_spojovy_seznam/Lss.cpp
@@ -1,39 +1,24 @@
-#include "Lss.h" 
+#include "Lss.h"
 #include <iostream>
 
 Lss::Lss()
-	: mPrvni(nullptr) {
-}
-Lss::~Lss() {}
-void Lss::pridejNazacatek(int data) {
+	: mPrvni(nullptr)
+{}
+Lss::~Lss(){}
+void Lss::pridejNazacatek(int data){
 	LssPrvek* novyPrvek = new LssPrvek();
 	novyPrvek->mHodnota = data;
 	novyPrvek->mDalsi = mPrvni;
 	mPrvni = novyPrvek;
 }
-void Lss::pridejDva(int data) {
-	std::cout<< "Prida prvek druhe misto v seznamu ";
-    LssPrvek* novyPrvek = new LssPrvek();
-    novyPrvek->mHodnota = data;
-    novyPrvek->mDalsi = mPrvni->mDalsi;
-    mPrvni->mDalsi = novyPrvek;
+void Lss::pridejNaDruh(int data){
+	std::cout << "Prida na druhou pozici ";
+	LssPrvek* novyPrvek = new LssPrvek();
+	novyPrvek->mHodnota = data;
+	novyPrvek->mDalsi = mPrvni->mDalsi;
+	mPrvni->mDalsi = novyPrvek;
 }
-void Lss::pridejKamkoliv(int data, int pozice){
-	std::cout << "Prida prvek na urcene misto v seznamu ";
-	LssPrvek* aktualni = mPrvni;
-	for (int i = 0; i < pozice - 1; i++)
-	{
-		if (aktualni->mDalsi == nullptr)
-			break;
-
-		aktualni = aktualni->mDalsi;
-	}
-	LssPrvek* novy = new LssPrvek();
-	novy->mHodnota = data;
-	novy->mDalsi = aktualni->mDalsi;
-	aktualni->mDalsi = novy;
-}
-void Lss::vypis() const {
+void Lss::vypis() const{
 	LssPrvek* aktualni = mPrvni;
 	while (aktualni != nullptr) {
 		std::cout << aktualni->mHodnota << " ";
@@ -41,9 +26,8 @@ void Lss::vypis() const {
 	}
 	std::cout << std::endl;
 }
-void Lss::vypisS() const {
+void Lss::vypisS() const{
 	LssPrvek* aktualni = mPrvni;
-	std::cout << "Sude hodnoty: ";
 	while (aktualni != nullptr) {
 		if (aktualni->mHodnota % 2 == 0) {
 			std::cout << aktualni->mHodnota << " ";
@@ -52,9 +36,8 @@ void Lss::vypisS() const {
 	}
 	std::cout << std::endl;
 }
-void Lss::vypisL() const {
+void Lss::vypisL() const{
 	LssPrvek* aktualni = mPrvni;
-	std::cout << "Liche hodnoty: ";
 	while (aktualni != nullptr) {
 		if (aktualni->mHodnota % 2 != 0) {
 			std::cout << aktualni->mHodnota << " ";
@@ -63,7 +46,7 @@ void Lss::vypisL() const {
 	}
 	std::cout << std::endl;
 }
-int Lss::soucet(){
+int Lss::soucet() {
 	LssPrvek* aktualni = mPrvni;
 	int soucet = 0;
 	while (aktualni != nullptr) {
@@ -71,15 +54,6 @@ int Lss::soucet(){
 		aktualni = aktualni->mDalsi;
 	}
 	return soucet;
-}
-int Lss::pocet() {
-	LssPrvek* aktualni = mPrvni;
-	int pocet = 0;
-	while (aktualni != nullptr) {
-		pocet++;
-		aktualni = aktualni->mDalsi;
-	}
-	return pocet;
 }
 int Lss::prumer() {
 	LssPrvek* aktualni = mPrvni;
@@ -92,6 +66,15 @@ int Lss::prumer() {
 	}
 	int prumer = soucet / pocet;
 	return prumer;
+}
+int Lss::pocet() {
+	LssPrvek* aktualni = mPrvni;
+	int pocet = 0;
+	while (aktualni != nullptr) {
+		pocet++;
+		aktualni = aktualni->mDalsi;
+	}
+	return pocet;
 }
 void Lss::zrusNaZacatku() {
 	LssPrvek* novyPrvek = new LssPrvek();
@@ -114,7 +97,7 @@ void Lss::zrusNaDruh() {
 }
 void Lss::zrusVse() {
 	LssPrvek* aktualni = mPrvni;
-	std::cout << "Smaze cely seznam ";
+	std::cout << "Smaze cely seznam";
 	while (aktualni != nullptr) {
 		LssPrvek* dalsi = aktualni->mDalsi;
 		delete aktualni;
