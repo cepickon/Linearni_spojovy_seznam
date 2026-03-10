e4e3ce05a05640678d807123233f83b4361bd83f
Jonáš <jonas.hlinak16@gmail.com>
2026-03-10 15:32:25 +0100

    Refactor output statements and modify list operations

--- Linearni_spojovy_seznam/Linearni_spojovy_seznam.cpp
+++ Linearni_spojovy_seznam/Linearni_spojovy_seznam.cpp
@@ -1,28 +1,31 @@
-﻿#include <iostream>
-#include "LssPrvek.h" 
+#include <iostream>
+#include "LssPrvek.h"
 #include "Lss.h"
 
 int main() {
 	Lss seznam;
+	std::cout << "Puvodni vypis cisel ";
 	seznam.pridejNazacatek(3);
 	seznam.pridejNazacatek(1);
 	seznam.pridejNazacatek(5);
 	seznam.pridejNazacatek(2);
 	seznam.pridejNazacatek(4);
 	seznam.vypis();
+	std::cout << "Vypis sudych cisel ";
 	seznam.vypisS();
+	std::cout << "Vypis lichych cisel ";
 	seznam.vypisL();
-	std::cout<< "Soucet: " << seznam.soucet() << std::endl;
-	std::cout << "Pocet: " << seznam.pocet() << std::endl;
-	std::cout << "Prumer: " << seznam.prumer() << std::endl;
+	std::cout << "Soucet:" << seznam.soucet() << std::endl;
+	std::cout << "Pocet:" << seznam.pocet() << std::endl;
+	std::cout << "Prumer:" << seznam.prumer() << std::endl;
 
-	seznam.pridejDva(0);
+	seznam.pridejNaDruh(6);
 	seznam.vypis();
-	seznam.pridejKamkoliv(6, 3);
-	seznam.vypis();
-
 	seznam.zrusNaZacatku();
 	seznam.vypis();
+	seznam.zrusNaDruh();
+	seznam.vypis();
 	seznam.zrusVse();
+	seznam.vypis();
 	std::cout << std::endl;
 }
